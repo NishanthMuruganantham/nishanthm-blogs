74963db36cd67794c10f3f3e1f13d7d5b9978729
NishanthMuruganantham <nishanthmurugananth10@gmail.com>
2026-03-02 20:29:15 +0000

    feat: project setup and integrations (#1)

---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import rehypePrettyCode from 'rehype-pretty-code';
+import rehypeMermaid from 'rehype-mermaid';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://nishanthm.dev',
+
+  integrations: [
+    mdx({
+      syntaxHighlight: false,
+      rehypePlugins: [
+        [rehypePrettyCode, { theme: 'one-dark-pro' }],
+        rehypeMermaid
+      ],
+    }),
+    sitemap(),
+    tailwind()
+  ]
+});
