19cee0c712ec1184b820497b5684c86d6efdd37e
NishanthMuruganantham <nishanthmurugananth10@gmail.com>
2026-03-25 19:10:09 +0000

    chore: update production domain to blogs.nishanthm.com (#11)

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nishanthm.dev',
+  site: 'https://blogs.nishanthm.com/',
 
   integrations: [
     mdx({
