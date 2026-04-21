cf08f77378330fae05a04d879d3b8e9e25571cd5
NishanthMuruganantham <nishanthmurugananth10@gmail.com>
2026-04-21 18:02:59 +0000

    feat: open external links in new tab via rehype-external-links

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import rehypePrettyCode from 'rehype-pretty-code';
+import rehypeExternalLinks from 'rehype-external-links';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
@@ -24,6 +25,10 @@ export default defineConfig({
             block: 'plaintext',
             inline: 'plaintext',
           },
+        }],
+        [rehypeExternalLinks, {
+          target: '_blank',
+          rel: ['nofollow', 'noopener', 'noreferrer']
         }]
       ],
     }),
